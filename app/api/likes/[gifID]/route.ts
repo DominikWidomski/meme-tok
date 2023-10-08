9afd8efb3cf1c09e2c088c74874c03c7148a777a
Dominik Widomski <dwidomski92@gmail.com>
2023-10-08 19:25:15 +0100

    feat: API likes integration

--- app/api/likes/[gifID]/route.ts
+++ app/api/likes/[gifID]/route.ts
@@ -1,23 +1,57 @@
 import prisma from "@/lib/prisma";
-import { cookies } from "next/headers";
 
-export type Params = Record<
-  "params",
-  {
+const USER_ID = process.env.USER_ID;
+if (!USER_ID) {
+  throw new Error("`USER_ID` env variable is missing.");
+}
+
+export type Params = {
+  params: {
     gifID: string;
-  }
->;
+  };
+};
 
 export async function POST(_: Request, { params }: Params) {
-  const cookieStore = cookies();
-  const user_id = cookieStore.get("userID")!.value;
-  const data = await prisma.likes.create({
-    data: { user_id, gif_id: params.gifID },
+  if (!USER_ID) {
+    throw new Error("`USER_ID` env variable is missing.");
+  }
+
+  const result = await prisma.likes.findFirst({
+    where: { user_id: USER_ID, gif_id: params.gifID },
   });
-  return Response.json({ data });
+
+  let data = null;
+  let action = null;
+
+  if (result === null) {
+    // NOTE: Upsert just in case seems safer
+    data = await prisma.likes.upsert({
+      create: { user_id: USER_ID, gif_id: params.gifID },
+      update: {},
+      where: {
+        user_id_gif_id: { user_id: USER_ID, gif_id: params.gifID },
+      },
+    });
+    action = "created";
+  } else {
+    data = await prisma.likes.delete({
+      where: { user_id_gif_id: { user_id: USER_ID, gif_id: params.gifID } },
+    });
+    action = "deleted";
+  }
+
+  console.log({ action, data });
+
+  return Response.json({ action, data });
 }
 
 export async function GET(_: Request, { params }: Params) {
-  const data = await prisma.likes.findMany({ where: { gif_id: params.gifID } });
-  return Response.json({ data });
+  const count = await prisma.likes.count({ where: { gif_id: params.gifID } });
+  const youLike = (await prisma.likes.findFirst({
+    where: { gif_id: params.gifID, user_id: USER_ID },
+  }))
+    ? true
+    : false;
+
+  return Response.json({ count, youLike });
 }
--- app/example/likes/page.tsx
+++ app/example/likes/page.tsx
@@ -1,20 +1,20 @@
 import MemeLikesView from "@/components/MemeLikesView";
+import type { likes } from "@prisma/client";
 
-const ExampleLikesPage = () => {
-  return (
-    <MemeLikesView
-      gifIds={[
-        "AAsj7jdrHjtp6",
-        "KY5SRpVZWMjD8R1VIj",
-        "yJFeycRK2DB4c",
-        "LRNxdA0soqs09YWa4F",
-        "zMCfqXkwjmTO8",
-        "W0c3xcZ3F1d0EYYb0f",
-        "l3q2K5jinAlChoCLS",
-        "12pWLnLOSggd9yzklS",
-      ]}
-    />
-  );
+const getLikes = async (): Promise<{ data: likes[] }> => {
+  const response = await fetch(`http://localhost:3000/api/likes`, {
+    cache: "no-cache",
+  });
+  const json = await response.json();
+  return json;
+};
+
+const ExampleLikesPage = async () => {
+  const { data } = await getLikes();
+
+  const ids = data.map(({ gif_id }) => gif_id);
+
+  return <MemeLikesView gifIds={ids} />;
 };
 
 export default ExampleLikesPage;
