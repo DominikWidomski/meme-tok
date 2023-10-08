201a2dc75a657303f09c70a00b334d4e961d3945
Dominik Widomski <dwidomski92@gmail.com>
2023-10-08 13:08:22 +0100

    feat: add workbench page

---
+++ app/workbench/page.tsx
@@ -0,0 +1,11 @@
+// import { Suspense } from "react";
+// import Loading from "./loading";
+// import MemeFeed from "./memeFeed";
+
+// const MemeFeed = lazy(() => import("./memeFeed"));
+
+import styles from "./page.module.scss";
+
+export default function Workbench() {
+  return <main className={styles.main}>HI</main>;
+}
